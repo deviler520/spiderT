24bc7ecdf9689a3e62dad4ff8deb944f6a771de6
gaopeng <devil_er@163.com>
2018-09-15 15:05:08 +0800

    。。。。。。not empty
    Signed-off-by: gaopeng <devil_er@163.com>

--- 11.py
+++ 11.py
@@ -1,16 +1,28 @@
 #encoding=utf-8
 
-import urllib.request as urllib2
+import urllib.request as request
+import bs4
 import io
 import sys
 
-def main():
-    url = r"http://hiphotos.baidu.com/error.html"
-    html = urllib2.urlopen(url).read()
-    print(html.decode("gb2312"))
+def main1():
+    url = r"http://www.hao6v.com/"
+     
+    page = request.urlopen(url).read().decode("gb2312")
+    page = bs4.BeautifulSoup(page,"html.parser")
+    hrefs = page.find("div", class_ = "col2").find('div', class_= "box").find_all("a")
+    for href in hrefs:
+        print(href["href"])
+        print(href.string)
+    
+    
+    
 
 
 
 if __name__ == "__main__":
+    '''
+    get http://www.hao6v.com/
     
-    main()
+    '''
+    main1()
